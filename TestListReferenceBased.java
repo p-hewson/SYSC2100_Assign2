f1df4f994fdabc1383fccb8a1567d38d470b3d7c
Paul Hewson <phewson97@gmail.com>
2017-02-09 16:04:47 -0500

    add remove method

--- TestListReferenceBased.java
+++ TestListReferenceBased.java
@@ -1,7 +1,6 @@
 public class TestListReferenceBased{
   private Node head;
   public void main(String args[]){
-
   }
 
   public void Add(Node node, int data, int index){
@@ -11,16 +10,38 @@ public class TestListReferenceBased{
       node.setNext(head);
       head = node;
     }
-
-    for(int x = 0; x < index; x++){
-      ptr = ptr.getNext();
-      prev = prev.getNext();
+    else{
+      for(int x = 0; x < index; x++){
+        ptr = ptr.getNext();
+        prev = prev.getNext();
+      }
+      node.setNext(ptr);
+      prev.setNext(node);
     }
-    node.setNext(ptr);
-    prev.setNext(node);
   }
 
+  public boolean isEmpty(Node node){
+    if(head == null) return true;
+    else return false;
+  }
 
+  public int size(Node node){
+    int count = 0;
+    for(Node ptr = head; ptr != null; ptr = ptr.getNext()){
+      count ++;
+    }
+    return count;
+  }
+
+  public void remove(int index){
+    Node ptr = head;
+    Node nxt = head.getNext().getNext();
+    for(int x = 0; x < index; x++){    //x may need to equal 1.
+      ptr = ptr.getNext();
+      nxt = nxt.getNext();
+    }
+    ptr.setNext(nxt);
+  }
 
 
   private class Node {
